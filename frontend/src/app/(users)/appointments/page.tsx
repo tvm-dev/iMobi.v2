890d14e1726c23b2865bb57b05e775dd33e25699
Yago <andradeyago56@gmail.com>
2025-04-23 12:55:15 -0300

    (route): Adding basic page to routes

--- frontend/src/app/(users)/appointments/page.tsx
+++ frontend/src/app/(users)/appointments/page.tsx
@@ -1,8 +1,8 @@
 export default function DashboardPage() {
   return (
     <main>
-      <h1>Auctioneers</h1>
-      <h2>Auct</h2>
+      <h1>Appointments</h1>
+      <h2>App</h2>
     </main>
   );
 }
---
+++ frontend/src/app/(users)/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <main>
+      <h1>Contact</h1>
+      <h2>Cont</h2>
+    </main>
+  );
+}
