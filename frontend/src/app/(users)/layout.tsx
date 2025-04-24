49f1b1e0e407e6e8cedb3412de1b481f1171859b
Yago <andradeyago56@gmail.com>
2025-04-24 17:35:31 -0300

    (layout): Defining a box for the application pages

--- frontend/src/app/(users)/layout.tsx
+++ frontend/src/app/(users)/layout.tsx
@@ -11,14 +11,16 @@ export default function RootLayout({
   const contactRef = useRef<HTMLDivElement>(null);
 
   return (
-    <main className='flex flex-col justify-between min-h-screen w-full'>
+    <main className='flex flex-col justify-between min-h-screen w-full '>
       <section>
         <Sidebar
           onContactClick={() =>
             contactRef.current?.scrollIntoView({ behavior: 'smooth' })
           }
         />
-        {children}
+        <div className='mx-auto p-4 bg-zinc-200 max-w-7xl h-full'>
+          {children}
+        </div>
       </section>
       <Footer ref={contactRef} />
     </main>
